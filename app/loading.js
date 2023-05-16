d72e107f0bbd1e2f48d691209a44bc0273957e06
Garrett Becker <garrettdbecker@gmail.com>
2023-05-16 19:21:02 -0400

    Updates

---
+++ app/loading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function LoadingPage() {
+  return (
+    <div id="loading-page">
+      <div className="text-center min-h-screen bg-navy font-cardoRegular">
+        <h3 className="text-gray text-lg py-32">Loading</h3>
+      </div>
+    </div>
+  )
+}
+
+export default LoadingPage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,44 @@
 /** @type {import('tailwindcss').Config} */
+
+/* ColorHunt palette link: https://colorhunt.co/palette/e8e2dbfab95bf5564e1a3263 */
+
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/flowbite/**/*.js",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    
   ],
   theme: {
+    colors: {
+      "white": "#FFFFFF",
+      "offWhite": "#F0F0F0",
+      "lightGrey": "#DBDBDB",
+      "grayishBlue": "#7B879D",
+      "darkBlue": "#1F3251",
+      "navy": "#1A3263",
+      "red": "#F5564E",
+      "orange": "#FAB95B",
+      "gray": "#E8E2DB",
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        aileronRegular: "Aileron-Regular",
+        aileronBold: "Aileron-Bold",
+        ddinRegular: "DDin-Regular",
+        ddinBold: "DDin-Bold",
+        cardoRegular: "Cardo-Regular",
+        cardoBold: "Cardo-Bold",
+        quattrocentoRegular: "Quattrocento-Regular",
+        quattrocentoBold: "Quattrocento-Bold",
       },
+      backgroundImage: {
+        'bgHome' : "url('/public/bg/bg-charlotte-sky.JPG')",
+      }
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin'),
+  ],
 }
